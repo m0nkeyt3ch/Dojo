e4c8dd3cf6660a325702062443ef4d8b000aafc6
monkeyt3ch <monkeytech34@gmail.com>
2021-06-05 04:11:29 +0700

    Complete guess game

--- rust_ground/week#1/guess_game/src/main.rs
+++ rust_ground/week#1/guess_game/src/main.rs
@@ -1,13 +1,33 @@
-use std::io;
+use std::{io, u32};
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Input your guess.");
+    let secret_num = rand::thread_rng().gen_range(1..101);
+    //println!("The secret number is: {}", secret_num);
+    loop {
+        println!("Input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("Your guess: {}", guess);
+        println!("Your guess: {}", guess);
+        
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("Complete guess number");
+                break;
+            }
+        }
+            
+    }
     
-    println!("Complete guess number");
 }
